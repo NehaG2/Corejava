ecbe88ada958cda6bef3c50c69a207751ed3f745
246559 <246559@DESKTOP-52SN321>
2022-05-12 14:40:09 +0530

    First commit

---
+++ core java/src/testpk/classobject.java
@@ -0,0 +1,34 @@
+package testpk;
+public class classobject 
+{
+	    int yearofpurchase;// data members
+	    String model;//data members
+	    double cost;//data members
+	   
+	    
+	    public void disp1()// Member function
+	    {
+	    	System.out.println("year of purchase"+2001);
+	    
+	    	
+	    }
+	public void disp2()
+	{
+	    System.out.println("model"+1999);
+	    System.out.println("cost"+200000);
+	}
+	    public static void main(String[] argo) 
+	    {
+	    	classobject hai=new classobject(); 
+	    	classobject bye=new classobject(); 
+	    	hai.disp1();
+	    	bye.disp2();
+	    	// TODO Auto-generated method stub
+	    	
+	    }
+}
+	    
+	    
+	    
+	   
+
---
+++ core java/src/testpk/dataabst.java
@@ -0,0 +1,21 @@
+package testpk;
+abstract class dataabst
+{
+	abstract void withdraw();
+	
+}
+
+class DataAbstraction extends atm
+{
+	
+ void withdraw()
+ {
+		System.out.println("withdraw successful");
+ }
+	
+public static void main(String args[])
+{
+	DataAbstraction ob1 =new DataAbstraction();
+	ob1.withdraw();
+}
+}
